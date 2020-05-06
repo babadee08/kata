e87c401fa86726f361cd6cb17020f43687706849
Damilare Durojaye <damilare.durojaye@gmail.com>
2020-05-06 21:02:06 +0200

    Refactored the generator method to use a for loop instead of a while loop and also cleaned up the test class

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -16,6 +16,7 @@ class PrimeFactorsTest extends TestCase
             [6, [2, 3]],
             [8, [2, 2, 2]],
             [100, [2, 2, 5, 5]],
+            [999, [3, 3, 3, 37]],
         ];
     }
 
@@ -28,29 +29,5 @@ class PrimeFactorsTest extends TestCase
     function it_tests_prime_factors(int $number, array $expected) {
         $factors = new PrimeFactors;
         $this->assertEquals($expected, $factors->generate($number));
-    }
-
-    /** @test */
-    function it_generates_prime_factors_for_1() {
-        $factors = new PrimeFactors;
-        $this->assertEquals([], $factors->generate(1));
-    }
-
-    /** @test */
-    function it_generates_prime_factors_for_2() {
-        $factors = new PrimeFactors;
-        $this->assertEquals([2], $factors->generate(2));
-    }
-
-    /** @test */
-    function it_generates_prime_factors_for_3() {
-        $factors = new PrimeFactors;
-        $this->assertEquals([3], $factors->generate(3));
-    }
-
-    /** @test */
-    function it_generates_prime_factors_for_4() {
-        $factors = new PrimeFactors;
-        $this->assertEquals([2, 2], $factors->generate(4));
     }
 }
